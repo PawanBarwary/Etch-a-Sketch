7b14c7d0035e1797b395699ce47bb6081a61e0ac
Pawan Barwary <pawan.bwy@gmail.com>
2022-01-14 19:10:43 +0100

    Switch over from flexbox to grid

--- script.js
+++ script.js
@@ -1,25 +1,44 @@
 const body = document.querySelector('body');
 const button = document.querySelector('button');
+const root = document.querySelector(':root');
 let container;
-const createGrid = (size) => {
-  container = document.createElement('div');
-  for(let i = 0; i<size; i++) {
-    let row = document.createElement('div');
-    for(let j = 0; j<size; j++) {
-      let div = document.createElement('div');
-      row.appendChild(div);
-      div.classList.add('grid-item');
-      div.addEventListener('mouseover', (event) => div.classList.add('hover'));
-      row.classList.add('row');
+
+const validateInput = (input) => {
+  console.log(typeof input);
+  if(typeof input != 'number') {
+    alert("You can only input numbers!");
+    return;
+  }
+  else {
+    if (input > 150) {
+      alert("Your grid needs to be under 150 squares wide");
+      return;
     }
-  container.appendChild(row);
+    return true;
+  }
+};
+
+const createGrid = (size) => {
+  if (validateInput(size) !== true) { 
+    const grid = parseInt(prompt("What size of grid would you like?"));
+    createGrid(grid);
+    return;
+  }
+  root.style.setProperty('--grid-size', size);
+  container = document.createElement('div');
+  container.classList.add('container');
+  for(let i = 0; i<size**2; i++) {
+    const div = document.createElement('div');
+    div.classList.add('grid-item');
+    div.addEventListener('mouseover', (event) => div.classList.add('hover'));
+    container.appendChild(div);
   }
   body.appendChild(container);
 };
 
 const clearGrid = () => {
   body.removeChild(container);
-  const grid = parseInt(prompt("how big grid fam?"));
+  const grid = parseInt(prompt("What size of grid would you like?"));
   createGrid(grid);
 };
 
